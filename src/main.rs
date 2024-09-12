b24f05d93c1af13f957d27916315b1b47786d863
AVKayen <aganitnt@gmail.com>
2024-09-13 00:34:22 +0200

    new file:   .gitignore 	new file:   Cargo.lock 	new file:   Cargo.toml 	new file:   src/main.rs

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+const SIZE: usize = 10001;
+
+fn calculate_collatz_length(n: usize, collatz_lengths: &mut Vec<usize>) {
+    let mut x = n;
+    let mut current_seq: Vec<usize> = vec![];
+
+    while collatz_lengths[x] == 0 {
+        current_seq.push(x);
+        if x % 2 == 0 {
+            x /= 2;
+        } else {
+            x = (3 * x + 1) / 2;
+        }
+
+        if collatz_lengths.len() < x + 1 {
+            collatz_lengths.resize(x + 1, 0);
+        }
+    }
+
+    for (idx, &num) in (current_seq).iter().rev().enumerate() {
+        collatz_lengths[num] = collatz_lengths[x] + idx + 1;
+    }
+}
+
+fn main() {
+    let mut collatz_lengths: Vec<usize> = vec![0; SIZE];
+    collatz_lengths[1] = 1;
+    for n in 2..SIZE {
+        calculate_collatz_length(n, &mut collatz_lengths)
+    }
+
+    collatz_lengths.truncate(SIZE);
+
+    println!("{}", collatz_lengths[2137])
+}
